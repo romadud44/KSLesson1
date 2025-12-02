d93491814e287af773947d829aa7bd3d764daeb3
roman <roman_crt-service@mail.ru>
2025-12-02 20:30:18 +0300

    KS-1-1 end of task

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -1,2 +1,8 @@
 package org.example.lesson1
 
+fun main(){
+    val numberOfOrders: Int = 75
+    val textThanksForPurchase: String = "СПАСИБО ЗА ПОКУПКУ!"
+    println(numberOfOrders)
+    println(textThanksForPurchase)
+}

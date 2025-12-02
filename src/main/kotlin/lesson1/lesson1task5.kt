929e790691e95e1b4e0ea2c11cd3446562bfc2c6
roman <roman_crt-service@mail.ru>
2025-12-02 21:35:24 +0300

    KS-1-5 end task

--- src/main/kotlin/lesson1/lesson1task5.kt
+++ src/main/kotlin/lesson1/lesson1task5.kt
@@ -1,2 +1,17 @@
 package org.example.lesson1
 
+import kotlin.time.Duration
+
+fun main() {
+    val seconds: Short = 6480
+    val hoursOfSpace: Int = seconds / 3600
+    val minutesOfSpace: Int = (seconds % 3600) / 60
+    val secondsOfSpace: Int = ((seconds % 3600) % 60) / 60
+    val secondsOfSpaceFinish: String
+    if (secondsOfSpace < 10) {
+        secondsOfSpaceFinish = "0$secondsOfSpace"
+    } else {
+        secondsOfSpaceFinish = secondsOfSpace.toString()
+    }
+    println("$hoursOfSpace:$minutesOfSpace:$secondsOfSpaceFinish")
+}

5f5284d95c3bce4213577bb24ca1baf250f42a6e
roman <roman_crt-service@mail.ru>
2025-12-02 20:50:45 +0300

    KS-1-3 end task

--- src/main/kotlin/lesson1/lesson1task3.kt
+++ src/main/kotlin/lesson1/lesson1task3.kt
@@ -1,2 +1,20 @@
 package org.example.lesson1
 
+fun main() {
+    val year: Short
+    year = 1961
+    var hour: Byte
+    var minute: Byte
+    hour = 9
+    minute = 7
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = 10
+    minute = 55
+    print(hour)
+    print(':')
+    println(minute)
+    println("$hour:$minute")
+}

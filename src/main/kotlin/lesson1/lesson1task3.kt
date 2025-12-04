307f2c664a1fe15464c8b79e5f30a4b6f7179c23
roman <roman_crt-service@mail.ru>
2025-12-04 16:12:26 +0300

    KS-1-5 correction task

--- src/main/kotlin/lesson1/lesson1task3.kt
+++ src/main/kotlin/lesson1/lesson1task3.kt
@@ -3,13 +3,13 @@ package org.example.lesson1
 fun main() {
     val year: Short
     year = 1961
-    var hour: Byte
-    var minute: Byte
+    var hour: Int
+    var minute: Int
     hour = 9
     minute = 7
     println(year)
-    println(hour)
-    println(minute)
+    println("0$hour")
+    println("0$minute")
 
     hour = 10
     minute = 55

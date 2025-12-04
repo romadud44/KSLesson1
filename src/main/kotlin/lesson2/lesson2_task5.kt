251a53eca40bb3572aef32924e2f73b61ddff318
roman <roman_crt-service@mail.ru>
2025-12-04 21:33:55 +0300

    KS-2-5 end task

--- src/main/kotlin/lesson2/lesson2_task5.kt
+++ src/main/kotlin/lesson2/lesson2_task5.kt
@@ -1,2 +1,11 @@
 package org.example.lesson2
 
+fun main() {
+    val deposit = 70000
+    val depositRate = 0.167
+    val сapitalization = (1 + (depositRate / 1))
+    val finishDeposit =
+        deposit * (сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization * сapitalization)
+
+    println("%.3f".format(finishDeposit))
+}

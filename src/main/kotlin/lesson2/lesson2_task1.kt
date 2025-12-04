d9cc82f41058de3b29fbb114a903e56888354511
roman <roman_crt-service@mail.ru>
2025-12-04 19:44:12 +0300

    KS-2-1 end task

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -1,2 +1,11 @@
 package org.example.lesson2
 
+fun main() {
+    val firstStudent: Double = 3.00
+    val secondStudent: Double = 4.00
+    val thirdStudent: Double = 3.00
+    val fourthStudent: Double = 5.00
+
+    val gpa = (firstStudent + secondStudent + thirdStudent + fourthStudent) / 4
+    println("Средний балл: $gpa")
+}

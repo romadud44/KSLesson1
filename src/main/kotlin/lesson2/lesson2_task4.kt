e4afb681d29bbf733587bb8a2d8dd15262b6c26b
roman <roman_crt-service@mail.ru>
2025-12-04 20:45:12 +0300

    KS-2-4 end task

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,2 +1,13 @@
 package org.example.lesson2
 
+fun main() {
+    val buff = 1.2f
+    val countCrystals = 7
+    val countIrons = 11
+
+    val countCrystalsBuff = (countCrystals * buff) - countCrystals
+    val countIronsBuff = (countIrons * buff) - countIrons
+
+    println("Бонусная кристалическая руда: " + countCrystalsBuff.toInt())
+    println("Бонусная железная руда: " + countIronsBuff.toInt())
+}

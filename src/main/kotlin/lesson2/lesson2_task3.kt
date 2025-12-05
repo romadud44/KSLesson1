1f4147643d5376142142c459401d115334986b36
roman <roman_crt-service@mail.ru>
2025-12-05 23:10:00 +0300

    KS-2-3 edit task

--- src/main/kotlin/lesson2/lesson2_task3.kt
+++ src/main/kotlin/lesson2/lesson2_task3.kt
@@ -4,11 +4,12 @@ fun main() {
     val hourDeparture = 9
     val minuteDeparture = 39
     val timeDeparture = 457
+    val minutesToHour = 60
 
-    val timeFromZero = (hourDeparture * 60) + minuteDeparture
+    val timeFromZero = (hourDeparture * minutesToHour) + minuteDeparture
     val sumTime = timeFromZero + timeDeparture
 
-    val hourArrival = sumTime / 60
-    val minuteArrival = sumTime % 60
+    val hourArrival = sumTime / minutesToHour
+    val minuteArrival = sumTime % minutesToHour
     println("Время прибытия: $hourArrival:$minuteArrival")
 }

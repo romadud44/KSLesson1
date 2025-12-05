58446c17854c1eee0471c42c902ee2336e508ba8
roman <roman_crt-service@mail.ru>
2025-12-06 00:05:11 +0300

    KS-3-3 end task

--- src/main/kotlin/lesson3/lesson3_task3.kt
+++ src/main/kotlin/lesson3/lesson3_task3.kt
@@ -1,2 +1,16 @@
 package org.example.lesson3
 
+fun main() {
+    val number = 5
+    println(
+        "$number X 1 = ${number * 1}\n" +
+                "$number X 2 = ${number * 2}\n" +
+                "$number X 3 = ${number * 3}\n" +
+                "$number X 4 = ${number * 4}\n" +
+                "$number X 5 = ${number * 5}\n" +
+                "$number X 6 = ${number * 6}\n" +
+                "$number X 7 = ${number * 7}\n" +
+                "$number X 8 = ${number * 8}\n" +
+                "$number X 9 = ${number * 9}\n"
+    )
+}

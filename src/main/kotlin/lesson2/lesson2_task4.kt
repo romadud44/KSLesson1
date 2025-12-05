215012d41ce123e22136a6d8cf7f0321f5e1942d
roman <roman_crt-service@mail.ru>
2025-12-05 23:13:24 +0300

    KS-2-4 edit task

--- src/main/kotlin/lesson2/lesson2_task4.kt
+++ src/main/kotlin/lesson2/lesson2_task4.kt
@@ -1,12 +1,13 @@
 package org.example.lesson2
 
 fun main() {
-    val buff = 1.2f
+    val buff = 20
+    val buffFinish: Float = 1+(buff/100f)
     val countCrystals = 7
     val countIrons = 11
 
-    val countCrystalsBuff = (countCrystals * buff) - countCrystals
-    val countIronsBuff = (countIrons * buff) - countIrons
+    val countCrystalsBuff = (countCrystals * buffFinish) - countCrystals
+    val countIronsBuff = (countIrons * buffFinish) - countIrons
 
     println("Бонусная кристалическая руда: " + countCrystalsBuff.toInt())
     println("Бонусная железная руда: " + countIronsBuff.toInt())

648c802c55c2c86cb0380d499e558276f85c7df6
roman <roman_crt-service@mail.ru>
2025-12-05 23:06:44 +0300

    KS-2-1 edit task

--- src/main/kotlin/lesson2/lesson2_task1.kt
+++ src/main/kotlin/lesson2/lesson2_task1.kt
@@ -5,7 +5,8 @@ fun main() {
     val secondStudent: Double = 4.00
     val thirdStudent: Double = 3.00
     val fourthStudent: Double = 5.00
+    val countNumbers: Int = 4
 
-    val gpa = (firstStudent + secondStudent + thirdStudent + fourthStudent) / 4
+    val gpa = (firstStudent + secondStudent + thirdStudent + fourthStudent) / countNumbers
     println("Средний балл: $gpa")
 }

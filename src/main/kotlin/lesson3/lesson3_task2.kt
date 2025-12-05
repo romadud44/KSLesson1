4eac360ba6a2e7c700f4526b948e0d30034d04e4
roman <roman_crt-service@mail.ru>
2025-12-05 23:58:01 +0300

    KS-3-2 end task

--- src/main/kotlin/lesson3/lesson3_task2.kt
+++ src/main/kotlin/lesson3/lesson3_task2.kt
@@ -1,2 +1,17 @@
 package org.example.lesson3
 
+fun main(){
+    val name = "Татьяна"
+    var lastName = "Андреева"
+    val secondName = "Сергеевна"
+    var age = 20
+
+    println("$lastName $name $secondName, $age лет")
+
+    lastName = "Сидорова"
+    age = 22
+
+    println("$lastName $name $secondName, $age лет")
+
+
+}

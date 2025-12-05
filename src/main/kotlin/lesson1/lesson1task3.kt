327313549fd6f0f4c3e8715a261cf19b8388f34f
roman <roman_crt-service@mail.ru>
2025-12-05 23:46:29 +0300

    KS-1-3 edit task

--- src/main/kotlin/lesson1/lesson1task3.kt
+++ src/main/kotlin/lesson1/lesson1task3.kt
@@ -3,17 +3,17 @@ package org.example.lesson1
 fun main() {
     val year: Short
     year = 1961
-    var hour: Int
-    var minute: Int
-    hour = 9
-    minute = 7
+    var hour: String
+    var minute: String
+    hour = "09"
+    minute = "07"
     println(year)
-    println("0$hour")
-    println("0$minute")
+    println(hour)
+    println(minute)
 
 
-    hour = 10
-    minute = 55
+    hour = "10"
+    minute = "55"
     print(hour)
     print(':')
     println(minute)

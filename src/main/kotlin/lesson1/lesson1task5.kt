fcf079330e3bc64380b3f7bf0195219265b55d39
roman <roman_crt-service@mail.ru>
2025-12-06 21:19:10 +0300

    KS-1-5 edit task

--- src/main/kotlin/lesson1/lesson1task5.kt
+++ src/main/kotlin/lesson1/lesson1task5.kt
@@ -1,13 +1,15 @@
 package org.example.lesson1
 
 import kotlin.time.Duration
+
 const val SECONDS_TO_HOUR = 3600
+const val SECONDS_TO_MINUTE = 60
 
 fun main() {
     val seconds: Short = 6480
     val hoursOfSpace: Int = seconds / SECONDS_TO_HOUR
 
-    var minutesOfSpace: Int = (seconds % SECONDS_TO_HOUR) / 60
+    var minutesOfSpace: Int = (seconds % SECONDS_TO_HOUR) / SECONDS_TO_MINUTE
     var secondsOfSpace: Int = ((seconds % SECONDS_TO_HOUR) % 60) / 60
     val secondsOfSpaceFinish: String
 //    if (secondsOfSpace < 10) {
@@ -15,8 +17,15 @@ fun main() {
 //    } else {
 //        secondsOfSpaceFinish = secondsOfSpace.toString()
 //    }
-    val minutesOfSpaceString = String.format("%02d", minutesOfSpace)
-    val secondsOfSpaceString = String.format("%02d", secondsOfSpace)
-    val hoursOfSpaceString = String.format("%02d", hoursOfSpace)
-    println("$hoursOfSpaceString:$minutesOfSpaceString:$secondsOfSpaceString")
+//    val minutesOfSpaceString = String.format("%02d", minutesOfSpace)
+//    val secondsOfSpaceString = String.format("%02d", secondsOfSpace)
+//    val hoursOfSpaceString = String.format("%02d", hoursOfSpace)
+    println(
+        "${String.format("%02d", hoursOfSpace)}:${String.format("%02d", minutesOfSpace)}:${
+            String.format(
+                "%02d",
+                secondsOfSpace
+            )
+        }"
+    )
 }

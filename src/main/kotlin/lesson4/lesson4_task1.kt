220c82417bd76fa8480ca351405c5c24fa00ed12
roman <roman_crt-service@mail.ru>
2025-12-06 22:19:14 +0300

    KS-4-1 end task

--- src/main/kotlin/lesson4/lesson4_task1.kt
+++ src/main/kotlin/lesson4/lesson4_task1.kt
@@ -1,2 +1,8 @@
 package org.example.lesson4
 
+fun main(){
+    val allTables = 13
+    val tablesOnToday = 13
+    val tablesOnTomorrow = 9
+    println("[Доступность столиков на сегодня: ${tablesOnToday < allTables}]\n[Доступность столиков на завтра: ${tablesOnTomorrow < allTables}]")
+}
